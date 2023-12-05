63218b185d62a0c086e72f0d8c2c4ccb25e849f0
root <root@DESKTOP-FDMLTH5.localdomain>
2023-12-05 17:52:30 -0600

    remove rundll32 setwindowposition

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
 using System.Timers;
 
 namespace PEXECutioner
@@ -19,24 +18,6 @@ namespace PEXECutioner
         {
             InitializeComponent();
         }
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-
-        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
-        const UInt32 SWP_NOSIZE = 0x0001;
-        const UInt32 SWP_NOMOVE = 0x0002;
-        const UInt32 SWP_NOZORDER = 0x0004;
-        const UInt32 SWP_NOREDRAW = 0x0008;
-        const UInt32 SWP_NOACTIVATE = 0x0010;
-        const UInt32 SWP_DRAWFRAME = 0x0020;
-        const UInt32 SWP_FRAMECHANGED = 0x0020;
-        const UInt32 SWP_SHOWWINDOW = 0x0040;
-        const UInt32 SWP_HIDEWINDOW = 0x0080;
-        const UInt32 SWP_NOCOPYBITS = 0x0100;
-        const UInt32 SWP_NOOWNERZORDER = 0x0200;
-        const UInt32 SWP_NOREPOSITION = 0x0200;
-        const UInt32 SWP_NOSENDCHANGING = 0x0400;
 
         private void PEXECutioner_Load(object sender, EventArgs e)
         {
@@ -67,7 +48,6 @@ namespace PEXECutioner
                 this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
                 this.BackgroundImage = mywallpaper;
                 this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-                SetWindowPos(Handle, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOREDRAW | SWP_NOOWNERZORDER | SWP_NOSENDCHANGING | SWP_NOACTIVATE);
             }
             else
             {
